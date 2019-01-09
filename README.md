c02beb380b6c8ca9fa08fc4431a33cf7fc78dbd7
spencercjh <shouspencercjh@foxmail.com>
2019-01-10 00:15:29 +0800

    unit test

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 my first python task
 
 http://git.tuiwen-tech.com/spencercjh/myFirstPython.git
+
+8a646ae5-4d11-48cf-b29f-e0575618c641
+
+b60b5cb4-9bea-4944-8bc2-1b31ef5f15bd
--- src/search_test.py
+++ src/search_test.py
@@ -6,9 +6,11 @@ from search_vip_resource import traverse_one_book, remove_file
 class TestSoup(unittest.TestCase):
 
     def test_search(self):
-        test_book_id = 'b60b5cb4-9bea-4944-8bc2-1b31ef5f15bd'
+        test_book_id = '8a646ae5-4d11-48cf-b29f-e0575618c641'
         remove_file()
-        self.assertTrue(traverse_one_book(test_book_id)[0])
+        result = traverse_one_book(test_book_id)
+        print(result)
+        self.assertTrue(result[0])
 
 
 if __name__ == '__main__':
